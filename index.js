c38b0e1f4d0f9a438f82d67e8e2a607fb5ef4fc8
MoisesRGK <moisesrogoski23000@gmail.com>
2022-09-28 10:36:29 -0300

    salavndo informaçoes dos serviços no banco de dados

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var bd = openDatabase("myA", "1.0", "Minha agenda", 4080);
+var listaDias = [];
+var listaServiços = [];
 
 bd.transaction(function (criar) {
     criar.executeSql(
-        "CREATE TABLE clientes (nome TEXT,telefone TEXT, email TEXT, segundaManha BIT, segundaTarde BIT, segundaNoite BIT, terçaManha BIT, terçaTarde BIT, terçaNoite BIT, quartaManha Bit, quartaTarde BIT, quartaNoite BIT, quintaManha BIT, quintaTarde BIT, quintaNoite BIT, sextaManha BIT, sextaTarde BIT, sextaNoite BIT, sabadoManha BIT)"
+        "CREATE TABLE clientes (nome TEXT,telefone TEXT, email TEXT, segundaManha BIT, segundaTarde BIT, segundaNoite BIT, terçaManha BIT, terçaTarde BIT, terçaNoite BIT, quartaManha Bit, quartaTarde BIT, quartaNoite BIT, quintaManha BIT, quintaTarde BIT, quintaNoite BIT, sextaManha BIT, sextaTarde BIT, sextaNoite BIT, sabadoManha BIT, Escova BIT, Hidratação BIT, Colorir BIT, Corte BIT, Colorimetria BIT, Maquiagem BIT, Depilação BIT, Manicure BIT)"
     );
 });
-var listaDias = [];
+
 for (let dias = 0; dias < 6; dias++) {
     listaDias.push([false, false, false]);
+}
+
+for (let nServiço = 0; nServiço < 8; nServiço++) {
+    listaServiços.push(false);
 }
 
 function upArrayInfos(nome, telefone, email) {
@@ -18,8 +24,12 @@ function upArrayInfos(nome, telefone, email) {
         }
     }
     arrayInfos.push(listaDias[5][0]);
+    for (let nServiço = 0; nServiço < 8; nServiço++) {
+        arrayInfos.push(listaServiços[nServiço]);
+    }
     return arrayInfos;
 }
+
 function salvarCliente() {
     window.location.reload(false);
     const nome = document.getElementById("nomeUsuario").value;
@@ -28,7 +38,7 @@ function salvarCliente() {
     let arrayInfos = upArrayInfos(nome, telefone, email);
     bd.transaction(function (inserir) {
         inserir.executeSql(
-            "INSERT INTO clientes (nome, telefone, email, segundaManha, segundaTarde, segundaNoite , terçaManha, terçaTarde, terçaNoite, quartaManha, quartaTarde, quartaNoite, quintaManha, quintaTarde, quintaNoite, sextaManha, sextaTarde, sextaNoite, sabadoManha) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            "INSERT INTO clientes (nome, telefone, email, segundaManha, segundaTarde, segundaNoite , terçaManha, terçaTarde, terçaNoite, quartaManha, quartaTarde, quartaNoite, quintaManha, quintaTarde, quintaNoite, sextaManha, sextaTarde, sextaNoite, sabadoManha, Escova, Hidratação, Colorir, Corte, Colorimetria, Maquiagem, Depilação, Manicure) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
             arrayInfos
         );
     });
@@ -48,3 +58,11 @@ function mudaStatusDia(id, nDia, nPeriodo) {
 function atualizaListaDias(nDia, nPeriodo) {
     listaDias[nDia][nPeriodo] = !listaDias[nDia][nPeriodo];
 }
+
+function mudaStatusServiços(id, nServiço) {
+    if (document.getElementById(id).checked) {
+        listaServiços[nServiço] = true;
+    } else {
+        listaServiços[nServiço] = false;
+    }
+};
